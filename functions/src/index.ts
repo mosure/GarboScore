8b6adf54f9a74366021b3778729c1cc63d7944b0
Mitchell Mosure <hctimit@gmail.com>
2019-10-19 17:01:38 -0500

    feat: added mongodb set

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,66 @@
 import * as functions from 'firebase-functions';
+import { MongoClient, Db } from 'mongodb';
+
+const MONGODB_URL = process.env.MONGODB_URL as string;
+const MONGODB_NAME = process.env.MONGODB_NAME as string;
+
+const mongoClient = new MongoClient(MONGODB_URL);
+
+const getMongoDB = (callback: (db: Db) => void) => {
+    mongoClient.connect(() => {
+        const db = mongoClient.db(MONGODB_NAME);
+
+        callback(db);
+      
+        mongoClient.close();
+    });
+};
+
+class SubmissionInterface {
+    address: string = '';
+    image: string = '';
+}
+
+const submissionValidator = new SubmissionInterface();
+
+const stripProperties = (obj: any, template: any) => {
+    for (const key in obj) {
+        if (!template[key]) {
+            delete obj[key];
+        }
+    }
+
+    return obj;
+};
 
 export const score = functions.https.onRequest((request, response) => {
-    response.send("Hello, this is the score function!");
+    if (request.method === 'POST') {
+        if (!(request.body instanceof SubmissionInterface)) {
+            response.status(400).send('Body must be of type: { address: string, image: string }');
+        }
+
+        const submission: SubmissionInterface = stripProperties(request.body, submissionValidator);
+
+        // TODO: Get a score from the ML
+        // This should either be an array of scores or cumulative score
+        const submissionScore = 0;
+
+        getMongoDB((db: Db) => {
+            const collection = db.collection('addresses');
+    
+            collection.updateOne({
+                address: submission.address,
+            }, {
+                '$set': {
+                    score: submissionScore
+                },
+            }, {
+                upsert: true,
+            });
+
+            response.status(201).send({ score: submissionScore });
+        });
+    }
+
+    response.status(404).send();
 });
