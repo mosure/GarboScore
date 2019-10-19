68f7ecee3649589510f366ac904e10a03a3fce9b
Mitchell Mosure <hctimit@gmail.com>
2019-10-19 18:59:20 -0500

    fix: merg

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD
-=======
 import { Box } from '@material-ui/core';
->>>>>>> 7682dbaf14a8c16f3b32c0b9a5bd309046fedb28
 
 const Home: React.FC = () => {
     return (
