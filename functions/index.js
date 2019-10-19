ba7b11e27f0b726c9f6694680530731d0ee38ef4
Mitchell Mosure <hctimit@gmail.com>
2019-10-19 15:16:54 -0500

    test push for backend

--- functions/index.js
+++ functions/index.js
@@ -5,6 +5,6 @@
  * @param {!express:Response} res HTTP response context.
  */
 exports.score = (req, res) => {
-    let message = req.query.message || req.body.message || 'Hello World NEW2!';
+    let message = req.query.message || req.body.message || 'Hello World NEW3!';
     res.status(200).send(message);
   };
