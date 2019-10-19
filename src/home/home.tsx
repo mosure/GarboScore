9176f40a9dd8037ed4c87ed29597b301b7b21ad9
Brady Lenz <bradylenz98@gmail.com>
2019-10-19 18:56:31 -0500

    feat: temp home content

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Box } from '@material-ui/core';
 
 const Home: React.FC = () => {
     return (
-        <>
+        <Box minHeight='100vh'>
             Home
-        </>
+        </Box>
     );
 }
 
