45bf9a152ac61f1121aa58193b46b48ac1f1051a
Mitchell Mosure <hctimit@gmail.com>
2019-10-19 13:17:04 -0500

    feat: added routing to home/demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Switch, Redirect, Route } from 'react-router-dom';
+
+import { Header, Footer } from './shared';
+import { Home } from './home';
+import { Demo } from './demo';
 
 const App: React.FC = () => {
     return (
         <>
-            THIS IS OUR APP
+            <Header/>
+
+            <Switch>
+                <Route exact path='/' component={Home}/>
+                <Route exact path='/demo' component={Demo}/>
+
+                <Redirect to='/'/>
+            </Switch>
+
+            <Footer/>
         </>
     );
 }
