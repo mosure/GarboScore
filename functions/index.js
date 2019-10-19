f234ad909ea84020f74df46f5117cc5345483a97
Brady Lenz <bradylenz98@gmail.com>
2019-10-19 14:47:28 -0500

    test

--- functions/index.js
+++ functions/index.js
@@ -5,6 +5,6 @@
  * @param {!express:Response} res HTTP response context.
  */
 exports.score = (req, res) => {
-    let message = req.query.message || req.body.message || 'Hello World NEW!';
+    let message = req.query.message || req.body.message || 'Hello World NEW2!';
     res.status(200).send(message);
   };
