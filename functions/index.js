a9b5c0e98628d604bd317c8caa0b1c4b5420ef01
Mitchell Mosure <hctimit@gmail.com>
2019-10-19 14:28:09 -0500

    feat: function test

--- functions/index.js
+++ functions/index.js
@@ -5,6 +5,6 @@
  * @param {!express:Response} res HTTP response context.
  */
 exports.score = (req, res) => {
-    let message = req.query.message || req.body.message || 'Hello World!';
+    let message = req.query.message || req.body.message || 'Hello World NEW!';
     res.status(200).send(message);
   };
