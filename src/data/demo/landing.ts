c6fd4029afaf5b2bf0194d268a2c3855063ec08f
Brady Lenz <bradylenz98@gmail.com>
2019-10-20 08:30:48 -0500

    feat: made tslint happy

--- src/data/demo/landing.ts
+++ src/data/demo/landing.ts
@@ -7,7 +7,7 @@ interface Demo {
 export const demoData: Demo = {
     title: 'Try It Out',
     description: `
-    Take a look at our pre-built \'X-ray\' images, or scroll down to upload one of your own.
+    Take a look at our pre-built 'X-ray' images, or scroll down to upload one of your own.
     `,
     presetsHeader: 'Pre-Built Images',
 };
