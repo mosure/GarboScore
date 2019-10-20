14c695b13d1886e83092323a06cb0308cae08d9f
Brady Lenz <bradylenz98@gmail.com>
2019-10-20 02:56:42 -0500

    feat: evaulation data

---
+++ src/data/demo/scores.ts
@@ -0,0 +1,116 @@
+interface Evaluation {
+    imgSrc: string;
+    imgAlt: string;
+    payload: ClassificationObject[];
+    isLoaded: boolean;
+}
+
+interface ClassificationObject {
+    displayName: string;
+    imageObjectDetection: ImageObjectDetection[];
+}
+
+interface ImageObjectDetection {
+    boundingBox: BoundingBox;
+    score: number;
+    color?: string;
+}
+
+interface BoundingBox {
+    normalizedVertices: NormalizedVertices[];
+}
+
+interface NormalizedVertices {
+    x: number;
+    y: number;
+}
+
+export const evalutionData: Evaluation[] = [
+    {
+        imgSrc: '',
+        imgAlt: '',
+        isLoaded: true,
+        payload: [
+            {
+                displayName: 'glass',
+                imageObjectDetection: [
+                    {
+                        boundingBox: {
+                            normalizedVertices: [
+                                {
+                                    x: 0.06722600013017654,
+                                    y: 0.032620400190353394,
+                                },
+                                {
+                                    x: 0.38055700063705444,
+                                    y: 0.5402039885520935,
+                                },
+                            ],
+                        },
+                        score: 0.9684039950370789,
+                    },
+                ],
+            },
+            {
+                displayName: 'plastic',
+                imageObjectDetection: [
+                    {
+                        boundingBox: {
+                            normalizedVertices: [
+                                {
+                                    x: 0.09706679731607437,
+                                    y: 0.5765519738197327,
+                                },
+                                {
+                                    x: 0.4439049959182739,
+                                    y: 0.9399960041046143,
+                                },
+                            ],
+                        },
+                        score: 0.9622189998626709,
+                    },
+                ],
+            },
+            {
+                displayName: 'metal',
+                imageObjectDetection: [
+                    {
+                        boundingBox: {
+                            normalizedVertices: [
+                                {
+                                    x: 0.5343419909477234,
+                                    y: 0.49305999279022217,
+                                },
+                                {
+                                    x: 0.9125480055809021,
+                                    y: 0.9828130006790161,
+                                },
+                            ],
+                        },
+                        score: 0.7150440216064453,
+                    },
+                ],
+            },
+            {
+                displayName: 'metal',
+                imageObjectDetection: [
+                    {
+                        boundingBox: {
+                            normalizedVertices: [
+                                {
+                                    x: 0.6127989888191223,
+                                    y: 0.04846449941396713,
+                                },
+                                {
+                                    x: 0.9485570192337036,
+                                    y: 0.5083829760551453,
+                                },
+                            ],
+                        },
+                        score: 0.5971580147743225,
+                    },
+                ],
+            },
+        ],
+    },
+];
