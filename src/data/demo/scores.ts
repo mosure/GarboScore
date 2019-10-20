6e0efd5e783f537ba0b7631d27cb7204cb76ebb6
Mitchell Mosure <hctimit@gmail.com>
2019-10-20 09:39:45 -0500

    fix: bounding boxes on pre rendered images

--- src/data/demo/scores.ts
+++ src/data/demo/scores.ts
@@ -115,7 +115,7 @@ export const evaluationData: Evaluation[] = [
         imgSrc: example2.default,
         imgAlt: 'Example 2',
         isLoaded: true,
-        score: 4,
+        score: 2,
         payload: [
             {
                 displayName: 'plastic',
@@ -159,24 +159,24 @@ export const evaluationData: Evaluation[] = [
         imgSrc: example3.default,
         imgAlt: 'Example 3',
         isLoaded: true,
-        score: 4,
+        score: 3,
         payload: [
             {
                 displayName: 'glass',
                 imageObjectDetection: {
                     boundingBox: {
-                        normalizedVertices: [
+                        'normalizedVertices': [
                             {
-                                x: 0.06722600013017654,
-                                y: 0.032620400190353394,
+                                'x': 0,
+                                'y': 0.45827698707580566,
                             },
                             {
-                                x: 0.38055700063705444,
-                                y: 0.5402039885520935,
+                                'x': 0.6068689823150635,
+                                'y': 0.6623250246047974,
                             },
                         ],
                     },
-                    score: 0.9684039950370789,
+                    score: 0.9809899926185608,
                 },
             },
             {
@@ -185,52 +185,34 @@ export const evaluationData: Evaluation[] = [
                     boundingBox: {
                         normalizedVertices: [
                             {
-                                x: 0.09706679731607437,
-                                y: 0.5765519738197327,
+                                x: 0.6239510178565979,
+                                y: 0.45372501015663147,
                             },
                             {
-                                x: 0.4439049959182739,
-                                y: 0.9399960041046143,
+                                x: 0.9885119795799255,
+                                y: 0.8156179785728455,
                             },
                         ],
                     },
-                    score: 0.9622189998626709,
+                    'score': 0.9476940035820007,
                 },
             },
             {
-                displayName: 'metal',
+                displayName: 'plastic',
                 imageObjectDetection: {
                     boundingBox: {
                         normalizedVertices: [
                             {
-                                x: 0.5343419909477234,
-                                y: 0.49305999279022217,
+                                x: 0.0421237014234066,
+                                y: 0.754764974117279,
                             },
                             {
-                                x: 0.9125480055809021,
-                                y: 0.9828130006790161,
+                                x: 0.5035750269889832,
+                                y: 0.9324280023574829,
                             },
                         ],
                     },
-                    score: 0.7150440216064453,
-                },
-            },
-            {
-                displayName: 'metal',
-                imageObjectDetection: {
-                    boundingBox: {
-                        normalizedVertices: [
-                            {
-                                x: 0.6127989888191223,
-                                y: 0.04846449941396713,
-                            },
-                            {
-                                x: 0.9485570192337036,
-                                y: 0.5083829760551453,
-                            },
-                        ],
-                    },
-                    score: 0.5971580147743225,
+                    score: 0.6781290173530579,
                 },
             },
         ],
