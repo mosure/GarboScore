d3326145d6e619a6f088ee864536496e0d02d5fe
Brady Lenz <bradylenz98@gmail.com>
2019-10-20 09:16:40 -0500

    fix: evaluation errors

--- src/demo/evaluation.tsx
+++ src/demo/evaluation.tsx
@@ -40,38 +40,42 @@ export const Evaluation: React.FC<IEvaluation> = (props: IEvaluation) => {
 
     return (
         <Box className={classes.container}>
-            <svg
-                height='100%'
-                width='100%'
-                xmlns='http://www.w3.org/2000/svg'
-                role='img'
-                viewBox='0 0 1 1'
-            >
-                <image height={1} width={1} href={props.imgSrc}/>
-                {
-                    props.payload.map((obj, index) => {
-                        const x = obj.imageObjectDetection[0].boundingBox.normalizedVertices[0].x;
-                        const y = obj.imageObjectDetection[0].boundingBox.normalizedVertices[0].y;
-                        const width = obj.imageObjectDetection[0].boundingBox.normalizedVertices[1].x;
-                        const height = obj.imageObjectDetection[0].boundingBox.normalizedVertices[1].y;
-                        return (
-                            <rect
-                                fill='black'
-                                key={index}
-                                fillOpacity={0.01}
-                                stroke={colorMapping[obj.displayName.toLowerCase()] || 'blue'}
-                                strokeWidth={0.005}
-                                x={x}
-                                y={y}
-                                width={width - x}
-                                height={height - y}
-                            >
-                                <title>{`${obj.displayName}: ${Math.round(obj.imageObjectDetection[0].score * 100) / 100}`}</title>
-                            </rect>
-                        );
-                    })
-                }
-            </svg>
+            {
+                (props.payload && props.payload[0].imageObjectDetection[0]) && (
+                    <svg
+                        height='100%'
+                        width='100%'
+                        xmlns='http://www.w3.org/2000/svg'
+                        role='img'
+                        viewBox='0 0 1 1'
+                    >
+                        <image height={1} width={1} href={props.imgSrc}/>
+                        {
+                            props.payload.map((obj, index) => {
+                                const x = obj.imageObjectDetection[0].boundingBox.normalizedVertices[0].x;
+                                const y = obj.imageObjectDetection[0].boundingBox.normalizedVertices[0].y;
+                                const width = obj.imageObjectDetection[0].boundingBox.normalizedVertices[1].x;
+                                const height = obj.imageObjectDetection[0].boundingBox.normalizedVertices[1].y;
+                                return (
+                                    <rect
+                                        fill='black'
+                                        key={index}
+                                        fillOpacity={0.01}
+                                        stroke={colorMapping[obj.displayName.toLowerCase()] || 'blue'}
+                                        strokeWidth={0.005}
+                                        x={x}
+                                        y={y}
+                                        width={width - x}
+                                        height={height - y}
+                                    >
+                                        <title>{`${obj.displayName}: ${Math.round(obj.imageObjectDetection[0].score * 100) / 100}`}</title>
+                                    </rect>
+                                );
+                            })
+                        }
+                    </svg>
+                )
+            }
             {
                 !props.isLoaded && props.imgSrc && (
                     <LinearProgress
