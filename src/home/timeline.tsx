677eabda14f170fd4c3d3f49e204429cd067cdc6
Brady Lenz <bradylenz98@gmail.com>
2019-10-20 01:45:38 -0500

    style: timeline step number margin update

--- src/home/timeline.tsx
+++ src/home/timeline.tsx
@@ -19,7 +19,7 @@ const useStyles = makeStyles(
         },
         stepHeader: {
             scrollMarginTop: 32,
-            marginBottom: 32,
+            marginBottom: 8,
             userSelect: 'none',
         },
         snapshotHeader: {
